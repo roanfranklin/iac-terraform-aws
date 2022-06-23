ca30441233284338330d75fa9679cafb728684bf
Roan Franklin <roanfranklin@gmail.com>
2022-06-23 10:59:10 -0300

    update variables VPC+Subnets in env.tfvars

--- 02-Network-DEV/var_custom.tf
+++ 02-Network-DEV/var_custom.tf
@@ -1,28 +1,2 @@
 # ################
-# VPC
-
-variable "vpc_env_cidr" {
-  description = "CIDR of VPC - Environment"
-}
-
-# ################
-# Subnets Private
-
-variable "subnet_private_az1" {
-  description = "Subnet Private - AZ A - Environment"
-}
-
-variable "subnet_private_az2" {
-  description = "Subnet Private - AZ B - Environment"
-}
-
-# ################
-# Subnets Public
-
-variable "subnet_public_az1" {
-  description = "Subnet Public - AZ A - Environment"
-}
-
-variable "subnet_public_az2" {
-  description = "Subnet Public - AZ B - Environment"
-}
+# Custom
--- 02-Network-DEV/vpc.tf
+++ 02-Network-DEV/vpc.tf
@@ -1,6 +1,6 @@
 # # Create a VPC
 resource "aws_vpc" "vpc_env" {
-  cidr_block           = var.cidr_env
+  cidr_block           = var.vpc_env_cidr
   
   enable_dns_hostnames = true
   enable_dns_support   = true
